632ac7acf9edba14996a75a401555c4bd2ec4ece
Sumeet Pal <palsumeet@seasiainfotech.com>
2022-06-23 11:44:58 +0530

    Testing

--- SwiftCICdDemo/AppDelegate.swift
+++ SwiftCICdDemo/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         //jdsehfrjkehdsfjkrhesdf
         //dhjdghhjdd
+        
+        //testing
         return true
     }
 
